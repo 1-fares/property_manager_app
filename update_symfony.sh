a008258a3ec46ed227f29192e4f5b08f478ebd83
Fares <klate@admin.casagrossmann.ch>
2018-10-17 21:31:16 +0000

    update_symfony.sh avoid OOM

--- update_symfony.sh
+++ update_symfony.sh
@@ -3,6 +3,6 @@
 /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048 && \
 /sbin/mkswap /var/swap.1 && \
 /sbin/swapon /var/swap.1 && \
-./composer update && \
+php -d memory_limit=-1 composer update && \
 /sbin/swapoff /var/swap.1 && \
 rm /var/swap.1
