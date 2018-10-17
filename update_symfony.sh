c08d1cbcf22fe4cbfa9078d15b9b8c9af184614b
Fares <klate@admin.casagrossmann.ch>
2018-10-17 19:34:23 +0000

    add update_symfony.sh

---
+++ update_symfony.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048 && \
+/sbin/mkswap /var/swap.1 && \
+/sbin/swapon /var/swap.1 && \
+./composer update && \
+/sbin/swapoff /var/swap.1 && \
+rm /var/swap.1
